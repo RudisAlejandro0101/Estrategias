00c2dcaace97946a7e0877144dc96454d62f26aa
RudisAlejandro0101 <106390777+RudisAlejandro0101@users.noreply.github.com>
2022-06-19 19:52:18 -0600

    Add files via upload

---
+++ Numero factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main (){
+	int num=20,factorial=1;
+	cout<<"Ingrese un numero";
+	cin>>num;
+	
+	for(int i=1;i<=num;i++){
+	
+		factorial=factorial*i;
+}
+	cout<<"El factorial del numero ingresado es:"<<factorial<<endl;
+	
+	system("pause");
+	return 0;
+}
